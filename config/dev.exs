ac0e0217c6ebc25726b129abcd7bcc77ae1e56c2
Lucas Bueno <buenolucasbr@gmail.com>
2020-10-13 21:16:09 -0300

    vai

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :phx_template, PhxTemplate.Repo,
-  username: "root",
-  password: "rootpw",
+  username: "circleci",
+  password: "circleci",
   database: "bookstore_test",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
