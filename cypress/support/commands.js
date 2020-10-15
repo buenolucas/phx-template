a975d6a6c391ceb2528ef9ff3c1dc66f3b48a6f0
Lucas Bueno <buenolucasbr@gmail.com>
2020-10-15 16:52:28 -0300

    wip

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,30 +11,3 @@
 //
 
 import l from "./locators";
-
-Cypress.Commands.add("login", (identity) => {
-  cy.visit("/login");
-  cy.get(l.login.id).type(identity);
-  //cy.get(l.login.password).type("123456");
-  cy.contains(l.login.submit, "Entrar").click();
-  cy.get(l.login.error).should("not.exist");
-  //console.log('dasdsdsa')
-  cy.url().should("not.contain", l.login.url);
-});
-
-Cypress.Commands.add("getToken", (identity) => {
-  return cy.request({
-    method: "POST",
-    url: Cypress.env("apiUrl"),
-    body: {
-      query:
-      `mutation {\n  loginWithToken(token: \"${identity}\") {\n    token\n  }\n}`,
-
-      variables:null
-    },
-  }).its('body.data.loginWithToken.token').should('not.be.empty')
-    .then((token) => {
-      console.log('xabçau')
-      return token
-    })
-});
