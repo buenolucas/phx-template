d5feafcffb263ba5e3a68e4424a23b1d06b84158
Lucas Bueno <buenolucasbr@gmail.com>
2020-10-16 09:19:53 -0300

    format

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ use Mix.Config
 config :phx_template, PhxTemplate.Repo,
   username: "root",
   password: "rootw",
-  #database: "phx_template_test#{System.get_env("MIX_TEST_PARTITION")}",
+  # database: "phx_template_test#{System.get_env("MIX_TEST_PARTITION")}",
   database: "bookstore_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
