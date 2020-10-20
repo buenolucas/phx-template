5174cb0a125f95018c2005045d14f0c9c0b6b316
Lucas Bueno <buenolucasbr@gmail.com>
2020-10-20 09:23:51 -0300

    xablay

--- cypress/integration/functonal-spec.js
+++ cypress/integration/functonal-spec.js
@@ -6,5 +6,11 @@ describe("Should be fuck", () => {
 
   it("should gonna be yeah!", () => {
     expect(true).to.equal(true);
+
+    cy.visit("https://duckduckgo.com/?t=newext&atb=v241-4__");
+    cy.get("#search_form_input_homepage").type(
+      "artilheiros do cruzeiro esporte clube"
+    );
+    cy.get("#search_button_homepage").click();
   });
 });
